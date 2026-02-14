57afa63661a08e01dbf2c7c0e972aec8bf91fec7
sibasish <sahusibasish.sahu@gmail.com>
2026-02-14 14:01:09 +0530

    moved content to root route for now

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
+import CrowdExplorer from "@/components/crowd-explorer/CrowdExplorer";
 import { TimeSlider } from "@/components/timeline/TimeSlider.client";
 //import IndiaMap from "@/components/map/IndiaMap.client";
+import InitialSnapshot from '@/data/crowd-snapshots/2025-jan.json'
 
 export default function Page() {
   return (
     <>
       {/* <IndiaMap /> */}
       {/* <TimeSlider /> */}
+      <CrowdExplorer initialData={InitialSnapshot.spots} />
     </>
   );
 }
